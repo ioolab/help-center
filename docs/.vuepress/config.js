c60d48fc388397de6564d91ae7377b5e26299788
ioolab <jacobwilliamsj041@gmail.com>
2024-04-25 23:36:50 +0800

    u

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,7 +14,7 @@ module.exports = {
     ],
     sidebar: [
         {
-            title: '最新域名',   // 必要的
+            title: '最新网址',   // 必要的
             path: '/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
             collapsable: false, // 可选的, 默认值是 true,
             sidebarDepth: 1,    // 可选的, 默认值是 1
