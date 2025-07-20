996bce4318e28eedbc2f53804fa41edfe3f1c0ac
ioolab <jacobwilliamsj041@gmail.com>
2025-07-20 22:53:26 +0800

    up

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,74 +1,37 @@
-module.exports = {
+import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from '@vuepress/theme-default'
+
+export default defineUserConfig({
   head: [
     ['link', { rel: 'icon', href: '/img/logo.png' }]
   ],
   title: '小羽帮助中心',
   description: '小羽帮助中心',
   base: '/help-center/',
-  themeConfig: {
+  theme: defaultTheme({
     logo: '/img/logo.png',
-    nav: [
+    navbar: [
         { text: '首页', link: '/' },
         // { text: 'Guide', link: '/guide/' },
         // { text: 'External', link: 'https://google.com' },
     ],
     sidebar: [
         {
-            title: '最新网址',   // 必要的
-            path: '/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-            collapsable: false, // 可选的, 默认值是 true,
-            sidebarDepth: 1,    // 可选的, 默认值是 1
+            text: '最新网址',   // 必要的
+            link: '/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+            collapsible: false, // 可选的, 默认值是 true,
           //   children: [
           //     '/'
           //   ]
         },
         {
-          title: '常见问题',   // 必要的
-          path: '/常见问题',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-          collapsable: false, // 可选的, 默认值是 true,
-          sidebarDepth: 1,    // 可选的, 默认值是 1
+          text: '常见问题',   // 必要的
+          link: '/常见问题',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+          collapsible: false, // 可选的, 默认值是 true,
         //   children: [
         //     '/'
         //   ]
         },
-        // {
-        //   title: '如何更新节点',
-        //   path: '/如何更新节点',
-        // //   children: [ /* ... */ ],
-        //   initialOpenGroupIndex: -1 // 可选的, 默认值是 0
-        // }
       ]
-    // sidebar: {
-    //     '/foo/': [
-    //         '',     /* /foo/ */
-    //         'one',  /* /foo/one.html */
-    //         'two'   /* /foo/two.html */
-    //     ],
-    
-    //     '/bar/': [
-    //         '',      /* /bar/ */
-    //         'three', /* /bar/three.html */
-    //         'four'   /* /bar/four.html */
-    //     ],
-
-    //     // '/Android/':[
-    //     //     '',
-    //     //     'client'
-    //     // ],
-    
-    //     // fallback
-    //     '/': [
-    //         '',        /* / */
-    //         'contact', /* /contact.html */
-    //         'about'    /* /about.html */
-    //     ],
-    // },
-    // sidebar: [
-    //     '/',
-    //     '/常见问题',
-    //     'Android',
-    //     ['/page-b', 'Explicit link text']
-    // ],
-    // displayAllHeaders: true
-  }
-}
+  })
+})
