1dfb9cb0dc05755c3934072daf5a50d0e8f6644f
ioolab <jacobwilliamsj041@gmail.com>
2025-07-20 23:47:25 +0800

    up

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,40 +1,36 @@
-import { defineUserConfig } from 'vuepress'
-import { defaultTheme } from '@vuepress/theme-default'
-import { viteBundler } from '@vuepress/bundler-vite'
-
-export default defineUserConfig({
-  bundler: viteBundler(),
+module.exports = {
   head: [
     ['link', { rel: 'icon', href: '/img/logo.png' }]
   ],
   title: '小羽帮助中心',
   description: '小羽帮助中心',
   base: '/help-center/',
-  dest: 'docs/.vuepress/dist',
-  theme: defaultTheme({
+  themeConfig: {
     logo: '/img/logo.png',
-    navbar: [
+    nav: [
         { text: '首页', link: '/' },
         // { text: 'Guide', link: '/guide/' },
         // { text: 'External', link: 'https://google.com' },
     ],
     sidebar: [
         {
-            text: '最新网址',   // 必要的
-            link: '/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-            collapsible: false, // 可选的, 默认值是 true,
+            title: '最新网址',   // 必要的
+            path: '/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+            collapsable: false, // 可选的, 默认值是 true,
+            sidebarDepth: 1,    // 可选的, 默认值是 1
           //   children: [
           //     '/'
           //   ]
         },
         {
-          text: '常见问题',   // 必要的
-          link: '/常见问题',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-          collapsible: false, // 可选的, 默认值是 true,
+          title: '常见问题',   // 必要的
+          path: '/常见问题',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+          collapsable: false, // 可选的, 默认值是 true,
+          sidebarDepth: 1,    // 可选的, 默认值是 1
         //   children: [
         //     '/'
         //   ]
         },
       ]
-  })
-})
+  }
+}
